3649f335fe9d5cb9d25f96030ff65aee2fb89197
Andras Schmelczer <andras@schmelczer.dev>
2025-03-16 10:32:29 +0000

    Delete fuzz

--- backend/fuzz/fuzz_targets/reconcile.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|texts: (String, String, String)| {
-    let (original, left, right) = texts;
-    let _ = reconcile::reconcile(&original, &left, &right);
-});
