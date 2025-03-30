71ccd7b61dd322127c96492675cd414e8dc6f33e
Andras Schmelczer <andras@schmelczer.dev>
2025-03-30 21:27:04 +0100

    Make Side printable

--- backend/reconcile/src/utils/side.rs
+++ backend/reconcile/src/utils/side.rs
@@ -1,4 +1,16 @@
+use std::fmt::Display;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Side {
     Left,
     Right,
+}
+
+impl Display for Side {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Side::Left => write!(f, "Left"),
+            Side::Right => write!(f, "Right"),
+        }
+    }
 }
