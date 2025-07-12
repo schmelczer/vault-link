d9d14e03e9fd437679650fefc47e8e16824e4d95
Andras Schmelczer <andras@schmelczer.dev>
2025-07-12 10:31:22 +0100

    Delete reconcile

--- backend/reconcile/src/tokenizer.rs
+++
@@ -1,6 +0,0 @@
-use token::Token;
-
-pub mod token;
-pub mod word_tokenizer;
-
-pub type Tokenizer<T> = dyn Fn(&str) -> Vec<Token<T>>;
--- backend/reconcile/src/utils.rs
+++
@@ -1,6 +0,0 @@
-pub mod common_prefix_len;
-pub mod common_suffix_len;
-pub mod find_longest_prefix_contained_within;
-pub mod merge_iters;
-pub mod side;
-pub mod string_builder;
