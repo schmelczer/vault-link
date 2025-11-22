4fcd134e55ebce8444af79b9e62f9e8ada280a3b
Andras Schmelczer <andras@schmelczer.dev>
2025-11-22 20:52:30 +0000

    Extract consts

--- frontend/sync-client/src/utils/is-file-type-mergable.ts
+++ frontend/sync-client/src/utils/is-file-type-mergable.ts
@@ -1,6 +1,8 @@
+import { MERGABLE_FILE_TYPES } from "../consts";
+
 export function isFileTypeMergable(pathOrFileName: string): boolean {
 	const parts = pathOrFileName.split(".");
 	const fileExtension = parts.at(-1) ?? "";
 
-	return ["md", "txt"].includes(fileExtension.toLowerCase());
+	return MERGABLE_FILE_TYPES.includes(fileExtension.toLowerCase());
 }
