31d4343fb1d3ede2170327912cf252ab243689a4
Andras Schmelczer <andras@schmelczer.dev>
2025-11-22 20:19:13 +0000

    Have the same error message for file not found

--- frontend/sync-client/src/file-operations/file-not-found-error.ts
+++ frontend/sync-client/src/file-operations/file-not-found-error.ts
@@ -1,5 +1,8 @@
 export class FileNotFoundError extends Error {
-	public constructor(message: string) {
+	public constructor(
+		message: string,
+		public readonly filePath: string
+	) {
 		super(message);
 		this.name = "FileNotFoundError";
 	}
