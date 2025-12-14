e103bba12c5a809b9e5f984e1077703969a4e1f5
Andras Schmelczer <andras@schmelczer.dev>
2025-12-14 17:08:37 +0000

    Don't depend on uuid

--- frontend/sync-client/src/utils/create-client-id.ts
+++ frontend/sync-client/src/utils/create-client-id.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
+
 
 export function createClientId(): string {
     // @ts-expect-error, injected by webpack
@@ -8,8 +8,8 @@ export function createClientId(): string {
         typeof navigator !== "undefined"
             ? navigator.platform // eslint-disable-line @typescript-eslint/no-deprecated
             : typeof process !== "undefined"
-              ? process.platform
-              : "unknown";
+                ? process.platform
+                : "unknown";
 
-    return `vault-link/${packageVersion} (${uuidv4()}; ${platform})`;
+    return `vault-link/${packageVersion} (${Math.round(Math.random() * 1e10)}; ${platform})`;
 }
